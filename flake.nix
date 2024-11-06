89e73a4fa8e98c870c1e5ccdcfaa5758f9d91f7d
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2024-11-06 22:50:03 +0800

    wip: update flakes for gazebo11 dependencies

--- flake.nix
+++ flake.nix
@@ -3,23 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=release-20.03";
-    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nix_unstable_pkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { nixpkgs, ... } @ inputs:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-  {
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      unstable_pkgs = inputs.nix_unstable_pkgs.legacyPackages.x86_64-linux;
+    in
+    {
 
-    packages.x86_64-linux.hello = pkgs.hello;
+      devShells.x86_64-linux.default = unstable_pkgs.mkShell {
+        buildInputs = [
+          unstable_pkgs.micromamba
+          unstable_pkgs.protobuf3_21
+          unstable_pkgs.cmake
+          unstable_pkgs.libsForQt5.full
+          unstable_pkgs.libsForQt5.qwt
+          unstable_pkgs.tinyxml
+          unstable_pkgs.freeimage
+          unstable_pkgs.libtar
+          unstable_pkgs.boost
+          pkgs.nodejs-10_x
+        ];
 
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = [
-        # pkgs.cowsay
-        pkgs.nodejs-10_x
-      ];
+      };
     };
-
-  };
 }
